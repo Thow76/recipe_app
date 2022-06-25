08de6bc08dc601ab6933e1bf2e7a7d327b628637
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-25 19:47:00 +0100

    4_setting_up_category_fields_constructor_for_item_view

---
+++ lib/category_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
