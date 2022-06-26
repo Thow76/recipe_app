93b83e03862f78a510e6a04beb5fb328900c1851
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-26 11:45:21 +0100

    14_164_Passing_Data_Via_Constructor_Passing_Category_Title_to_Category_Screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -3,16 +3,17 @@ import 'package:recipe_app/category_meals.dart';
 import 'category_meals.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryMealsScreen();
+          return CategoryMealsScreen(id, title);
         },
       ),
     );
