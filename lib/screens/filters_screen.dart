c0d9c417c558d488d1fb933ec72c738d97b9d04f
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-29 17:06:53 +0100

    31_178_Setting_Up_Page_Structure

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -8,8 +8,10 @@ class FiltersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters'),
+    return Scaffold(
+      body: Center(
+        child: Text('Filters'),
+      ),
     );
   }
 }
