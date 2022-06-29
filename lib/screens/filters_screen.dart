3bc27a7ee740b37e2a956e1b057faf021f962f1d
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-29 17:12:41 +0100

    33_178_Implementing_Drawer_On_Page

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:flutter/material.dart';
+import 'package:recipe_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
@@ -12,6 +13,7 @@ class FiltersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Filters'),
       ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Filters'),
       ),
